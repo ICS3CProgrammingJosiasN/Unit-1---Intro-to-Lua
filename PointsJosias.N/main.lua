1b1473c1d025a0d6c9076e80e8037304f437f0cb
s456389 <josias.nguessan@ocsbstudent.ca>
2019-10-21 14:23:54 -0400

    new

--- PointsJosias.N/main.lua
+++ PointsJosias.N/main.lua
@@ -27,17 +27,8 @@ local userAnswer
 local correctAnswer
 local points = 0
 
--- variables for the timer
-local totalSeconds = 15
-local secondsLeft = 15
-local clockText
-local countDownTimer
 
-local lives = 4
-local heart1
-local heart2
-local heart3
-local heart4
+
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
 -- LOCAL FUNCTIONS
@@ -52,21 +43,6 @@ local function AskQuestion()
 
 	-- create question in the next text object
 	questionObject.text = randomNumber1 .. "+" .. randomNumber2
-	
-	correctAnswer = randomNumber1 - randomNumber2
-
-	-- create question in the next text object
-	questionObject.text = randomNumber1 .. "-" .. randomNumber2	
-
-	correctAnswer = randomNumber1 * randomNumber2
-
-	-- create question in the next text object
-	questionObject.text = randomNumber1 .. "x" .. randomNumber2	
-
-	correctAnswer = randomNumber1 / randomNumber2
-
-	-- create question in the next text object
-	questionObject.text = randomNumber1 .. "÷" .. randomNumber2
 
 end	
 
@@ -108,68 +84,20 @@ local function NumericFieldListener( event )
             pointsText.text = "Points = " .. points 
 
         else
-          lives = lives - 1
           print ("*****testing")
           incorrectObject.isVisible = true 
           timer.performWithDelay(2000, Hideincorrect)
-
-            if (lives == 3) then
-              	heart4.isVisible = false
-             
-            elseif (lives == 2) then
-				heart3.isVisible = false
-
-		    elseif (lives == 1) then
-			 	heart2.isVisible = false
-
-		    elseif (lives == 0) then 
-				heart1.isVisible = false 
-		 	end 
-		  
-                
-
-
-
-
-         end
+        end
 
      end
      
 end
 
-local function UpdateTime()
-	-- decrement the number of sceonds 
-	secondsLeft = secondsLeft - 1
 
-	-- display the number of seconds left in the clock object 
-	clockText.text = "Time Left = " .. secondsleft .. ""
 
-	if (secondsleft == 0 ) then 
-		-- reset the number of seconds left 
-		secondsLeft = totalSeconds
-		lives = lives - 1
 
-		-- *** IF THERE ARE NO LIVES LEFT, PLAY A LOSE SOUND, AND SHOW A YOU LOSE IIMAGE
-		-- AND CANCEL THE TIMER REMOVE THE THIRD HEART BY MAKING IT INVISIBLE 
-		if (lives == 3) then
-			heart4.isVisible = false
 
-		elseif (lives == 2) then
-			heart2.isVisible = false
 
-		elseif (lives == 1) then 
-			heart1.isVisible = false 
-		end 
-		
-		-- *** CALL THE FUNCTION TO ASK A NEW QUESTION
-	end
-end
-
--- function that calls the timer 
-local function StartTimer()
-	-- create a countdown timer that loops infinitely 
-	countDownTimer = timer.performWithDelay( 1000, UpdateTime, 0)
-end	
 
 
 
@@ -201,24 +129,7 @@ numericField:addEventListener( "userInput", NumericFieldListener)
 -- display the amount of points as text object
 pointsText = display.newText("Points = " .. points, display.contentWidth/3, display.contentHeight/3, nil,50)
 
--- create the lives to display ont the screen 
-heart1 = display.newImageRect("Images/heart.png", 100, 100)
-heart1.x = display.contentWidth  * 7/8
-heart1.y = display.contentHeight * 1/7
 
-heart2 = display.newImageRect("Images/heart.png", 100, 100)
-heart2.x = display.contentWidth * 6/8
-heart2.y = display.contentHeight * 1/7
-
-heart3 = display.newImageRect("Images/heart.png", 100, 100)
-heart3.x = display.contentWidth * 3.5/5.5
-heart3.y = display.contentHeight * 1/7
-
-heart4 = display.newImageRect("Images/heart.png", 100, 100)
-heart4.x = display.contentWidth * 1.5/2.9
-heart4.y = display.contentHeight * 1/7
-
-clockText = display.newText("Time Left= " .. secondsLeft, display.contentWidth*1/8, display.contentHeight*1/7, nil, 50)
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- FUNCTION CALLS
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
